857dad233b980e5ea553d2e60294fac23631e89e
Jan Hackel <jhunovis+github@googlemail.com>
2012-08-30 12:45:34 +0200

    Test cases demonstrating simple tasks with hibernate. Also hilites noteworthy quirks.

---
+++ hibernate/src/test/java/jhunovis/experiments/hibernate/piclib/BaseTests.java
@@ -0,0 +1,33 @@
+package jhunovis.experiments.hibernate.piclib;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class BaseTests {
+
+	protected static SessionFactory mSessionFactory;
+	protected final Logger logger = LoggerFactory
+				.getLogger(BasicTests.class);
+
+	@BeforeClass
+	@SuppressWarnings("deprecation")
+	public static void setupSessioFactory() {
+		mSessionFactory = new Configuration().configure().buildSessionFactory();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		if (mSessionFactory != null) {
+			mSessionFactory.close();
+		}
+	}
+
+	public BaseTests() {
+		super();
+	}
+
+}

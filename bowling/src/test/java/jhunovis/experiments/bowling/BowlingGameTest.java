d98e6a264da73f6dbefba26b9001e9ca44e73837
Jan Hackel <jhunovis+github@googlemail.com>
2012-09-06 23:47:22 +0200

    Further splitting of gameProgess. Now isolating utility for rolling ten frames.

--- bowling/src/test/java/jhunovis/experiments/bowling/BowlingGameTest.java
+++ bowling/src/test/java/jhunovis/experiments/bowling/BowlingGameTest.java
@@ -5,27 +5,27 @@ import org.junit.Test;
 
 public class BowlingGameTest {
 
-	@Test(expected=BowlingException.class)
+	@Test(expected = BowlingException.class)
 	public void invalidRollBelowZero() throws BowlingException {
 		BowlingGame game = new BowlingGame();
 		game.addRoll(-1);
 	}
 
-	@Test(expected=BowlingException.class)
-	public void invalidRollAboveTen()  throws BowlingException {
+	@Test(expected = BowlingException.class)
+	public void invalidRollAboveTen() throws BowlingException {
 		BowlingGame game = new BowlingGame();
 		game.addRoll(11);
 	}
 
-	@Test(expected=BowlingException.class)
+	@Test(expected = BowlingException.class)
 	public void invalidSecondRollBelowZero() throws BowlingException {
 		BowlingGame game = new BowlingGame();
 		game.addRoll(2);
 		game.addRoll(-2);
 	}
 
-	@Test(expected=BowlingException.class)
-	public void invalidFrameAboveTen()  throws BowlingException {
+	@Test(expected = BowlingException.class)
+	public void invalidFrameAboveTen() throws BowlingException {
 		BowlingGame game = new BowlingGame();
 		game.addRoll(2);
 		game.addRoll(9);
@@ -57,21 +57,32 @@ public class BowlingGameTest {
 		assertEquals(1, game.getCurrentRoll());
 		return game;
 	}
-	
+
 	@Test
 	public void firstNinesFrameNoStrikeNoSpare() throws BowlingException {
 		rollNineFrames();
 	}
-	
-	@Test
-	public void tenthFrameIsStrike() throws BowlingException {
+
+	protected BowlingGame rollTenFramesLastFrameIsStrike()
+			throws BowlingException {
 		BowlingGame game = rollNineFrames();
 
 		game.addRoll(10);
 		assertTrue(!game.isComplete());
-
 		assertEquals(10, game.getCurrentFrame());
 		assertEquals(2, game.getCurrentRoll());
+		return game;
+	}
+
+	@Test
+	public void gameCompleteAfterTenthFrameIsStrike() throws BowlingException {
+		rollTenFramesLastFrameIsStrike();
+	}
+	
+	@Test
+	public void strikesAfterTenthFrameIsStrike() throws BowlingException {
+		BowlingGame game = rollTenFramesLastFrameIsStrike();
+
 		game.addRoll(10);
 		assertTrue(!game.isComplete());
 
@@ -82,7 +93,33 @@ public class BowlingGameTest {
 	}
 
 	@Test
-	public void tenthFrameIsSpare() throws BowlingException {
+	public void spareAfterTenthFrameIsStrike() throws BowlingException {
+		BowlingGame game = rollTenFramesLastFrameIsStrike();
+
+		game.addRoll(4);
+		assertTrue(!game.isComplete());
+
+		assertEquals(10, game.getCurrentFrame());
+		assertEquals(3, game.getCurrentRoll());
+		game.addRoll(6);
+		assertTrue(game.isComplete());
+	}
+
+	@Test
+	public void notClearedAfterTenthFrameIsStrike() throws BowlingException {
+		BowlingGame game = rollTenFramesLastFrameIsStrike();
+
+		game.addRoll(4);
+		assertTrue(!game.isComplete());
+
+		assertEquals(10, game.getCurrentFrame());
+		assertEquals(3, game.getCurrentRoll());
+		game.addRoll(3);
+		assertTrue(game.isComplete());
+	}
+
+	protected BowlingGame rollTenFramesLastFrameIsSpare()
+			throws BowlingException {
 		BowlingGame game = rollNineFrames();
 
 		game.addRoll(4);
@@ -90,16 +127,35 @@ public class BowlingGameTest {
 
 		assertEquals(2, game.getCurrentRoll());
 		game.addRoll(6);
-		assertTrue(!game.isComplete());
 
+		assertTrue(!game.isComplete());
 		assertEquals(10, game.getCurrentFrame());
 		assertEquals(3, game.getCurrentRoll());
+		return game;
+	}
+
+	@Test
+	public void gameCompleteAfterSpareInLastFrame() throws BowlingException {
+		rollTenFramesLastFrameIsSpare();
+	}
+
+	@Test
+	public void noStrikeAfterSpareInLastFrame() throws BowlingException {
+		BowlingGame game = rollTenFramesLastFrameIsSpare();
 		game.addRoll(7);
 		assertTrue(game.isComplete());
 	}
 
 	@Test
-	public void tenthFrameNoSpareNorStrike() throws BowlingException {
+	public void strikeAfterSpareInLastFrame() throws BowlingException {
+		BowlingGame game = rollTenFramesLastFrameIsSpare();
+
+		game.addRoll(10);
+		assertTrue(game.isComplete());
+	}
+
+	protected void rollTenFramesLastFrameIsNeitherStrikeNorSpare()
+			throws BowlingException {
 		BowlingGame game = rollNineFrames();
 
 		game.addRoll(4);
@@ -108,6 +164,12 @@ public class BowlingGameTest {
 		assertEquals(2, game.getCurrentRoll());
 		game.addRoll(5);
 		assertTrue(game.isComplete());
+	}
+
+	@Test
+	public void gameCompleteAfterTenthFrameNoSpareNorStrike()
+			throws BowlingException {
+		rollTenFramesLastFrameIsNeitherStrikeNorSpare();
 	}
 
 	@Test

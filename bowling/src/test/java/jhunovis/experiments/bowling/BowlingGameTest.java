1cc49d1e42670aaf57b05298da1f9317de105715
Jan Hackel <jhunovis+github@googlemail.com>
2012-09-07 09:58:21 +0200

    Test-case «score» split into 11 test-cases.

--- bowling/src/test/java/jhunovis/experiments/bowling/BowlingGameTest.java
+++ bowling/src/test/java/jhunovis/experiments/bowling/BowlingGameTest.java
@@ -78,7 +78,7 @@ public class BowlingGameTest {
 	public void gameCompleteAfterTenthFrameIsStrike() throws BowlingException {
 		rollTenFramesLastFrameIsStrike();
 	}
-	
+
 	@Test
 	public void strikesAfterTenthFrameIsStrike() throws BowlingException {
 		BowlingGame game = rollTenFramesLastFrameIsStrike();
@@ -201,49 +201,79 @@ public class BowlingGameTest {
 	}
 
 	@Test
-	public void score() throws BowlingException {
+	public void scoreAllStrikes() throws BowlingException {
 		BowlingGame game = new BowlingGame(new int[] { 10, 10, 10, 10, 10, 10,
 				10, 10, 10, 10, 10, 10 });
 		assertEquals(300, game.getScore());
+	}
 
-		game = new BowlingGame(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 0 });
+	@Test
+	public void scoreAllZero() throws BowlingException {
+		BowlingGame game = new BowlingGame(new int[] { 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
 		assertEquals(0, game.getScore());
+	}
 
-		game = new BowlingGame(new int[] { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2,
-				3, 4, 5, 1, 2, 3, 4, 5 });
+	@Test
+	public void scoreNeitherStrikeNorSpare() throws BowlingException {
+		BowlingGame game = new BowlingGame(new int[] { 1, 2, 3, 4, 5, 1, 2, 3,
+				4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 });
 		assertEquals(60, game.getScore());
+	}
 
-		game = new BowlingGame(new int[] { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2,
-				3, 4, 5, 1, 2, 3, 5, 5, 10 });
+	@Test
+	public void scoreLastSpareExtraRollIsStrike() throws BowlingException {
+		BowlingGame game = new BowlingGame(new int[] { 1, 2, 3, 4, 5, 1, 2, 3,
+				4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 5, 5, 10 });
 		assertEquals(71, game.getScore());
+	}
 
-		game = new BowlingGame(new int[] { 0, 10, 1, 9, 2, 8, 3, 7, 4, 6, 5, 5,
-				6, 4, 7, 3, 8, 2, 9, 1, 10 });
+	@Test
+	public void scoreAllSparesExtraRollIsStrike() throws BowlingException {
+		BowlingGame game = new BowlingGame(new int[] { 0, 10, 1, 9, 2, 8, 3, 7,
+				4, 6, 5, 5, 6, 4, 7, 3, 8, 2, 9, 1, 10 });
 		assertEquals(155, game.getScore());
+	}
 
-		// last a strike
-		game = new BowlingGame(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 10, 10, 10 });
+	@Test
+	public void scoreAllZeroLastFrameStrikeExtraRollsStrikes()
+			throws BowlingException {
+		BowlingGame game = new BowlingGame(new int[] { 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10 });
 		assertEquals(30, game.getScore());
+	}
 
-		// last a spare
-		game = new BowlingGame(new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 5, 5, 7 });
+	@Test
+	public void scoreAllZeroLastFrameSpare() throws BowlingException {
+		BowlingGame game = new BowlingGame(new int[] { 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 7 });
 		assertEquals(17, game.getScore());
+	}
 
-		// incomplete games
-
-		game = new BowlingGame(new int[] { 10, 10, 10 });
+	@Test
+	public void midGameScoreAllStrikes() throws BowlingException {
+		BowlingGame game = new BowlingGame(new int[] { 10, 10, 10 });
 		assertEquals(60, game.getScore());
+	}
 
-		game = new BowlingGame(new int[] { 10, 10, 10, 5 });
+	@Test
+	public void midGameScoreInterruptedStrikeStreak() throws BowlingException {
+		BowlingGame game = new BowlingGame(new int[] { 10, 10, 10, 5 });
 		assertEquals(75, game.getScore());
+	}
 
-		game = new BowlingGame(new int[] { 1, 2, 3, 4 });
+	@Test
+	public void midGameScoreNeitherStrikeNorSpare() throws BowlingException {
+		BowlingGame game = new BowlingGame(new int[] { 1, 2, 3, 4 });
 		assertEquals(10, game.getScore());
 
 		game = new BowlingGame(new int[] { 3, 7, 2, 8, 1 });
+		assertEquals(24, game.getScore());
+	}
+
+	@Test
+	public void midGameScoreTwoSpares() throws BowlingException {
+		BowlingGame game = new BowlingGame(new int[] { 3, 7, 2, 8, 1 });
 		assertEquals(24, game.getScore());
 	}
 
